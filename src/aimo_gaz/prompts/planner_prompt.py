f5072c99c4fa0c26f7e5a43216773d5c42e99a74
George Tsoukalas <georgetsoukalaspc@gmail.com>
2024-06-20 20:27:14 +0000

    Added planner agent prompt and solver, other typo fixes.

---
+++ src/aimo_gaz/prompts/planner_prompt.py
@@ -0,0 +1,15 @@
+from aimo_gaz.prompts.prompt import ConcatPrompt
+
+class PlannerPrompt(ConcatPrompt):
+    def __init__(self, system_prompt_path: str = None, example_prompt_path: str = None, system_prompt: str = None, example_prompt: str = None):
+        super().__init__(system_prompt_path, example_prompt_path, system_prompt, example_prompt)
+        self.system_prompt = """
+Below is a math problem you are to solve. Give a high-level strategy for how to solve this problem, omitting any computations.    
+"""
+    def get_prompt(self, messages: list[dict[str, str]]) -> str:
+        message = list(messages[0].values())[0]
+        full_prompt = self.system_prompt.format(message, '{}')
+        return full_prompt
+    
+    def parse_response(self, response: str) -> str:
+        return response
--- src/aimo_gaz/prompts/system_prompts/test_system_prompt.md
+++ src/aimo_gaz/prompts/system_prompts/test_system_prompt.md
@@ -1,5 +1,3 @@
-Given a math problem give the final answer to that problem.
-The answer is always a number between 100 and 999. Make sure to return answer in that range.
-Use '[END]' to indicate the end of the answer.
+Given a math problem give the final answer to that problem. Use '[END]' to indicate the end of the answer.
 
 Example:
