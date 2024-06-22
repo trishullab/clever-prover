c3ac0c13bfba970c1f42a928524553c931a1fc06
zayne-sprague <zayne_sprague@yahoo.com>
2024-06-21 20:49:32 -0500

    prompt changes

--- src/aimo_gaz/prompts/planner_prompt.py
+++ src/aimo_gaz/prompts/planner_prompt.py
@@ -1,16 +1,24 @@
 from aimo_gaz.prompts.prompt import ConcatPrompt
 
 class PlannerPrompt(ConcatPrompt):
-    def __init__(self, system_prompt_path: str = None, example_prompt_path: str = None, system_prompt: str = None, example_prompt: str = None):
-        super().__init__(system_prompt_path, example_prompt_path, system_prompt, example_prompt)
+    def __init__(self, system_prompt_path: str = None, example_prompt_path: str = None, system_prompt: str = None, example_prompt: str = None,  append_system_prompt_after_every_message: bool = False):
+        super().__init__(system_prompt_path, example_prompt_path, system_prompt, example_prompt, append_system_prompt_after_every_message)
         self.system_prompt = """
 Below is a math problem you are to solve. Give a high-level strategy for how to solve this problem, omitting any computations.
 {}
 """
     def get_prompt(self, messages: list[dict[str, str]]) -> str:
-        message = list(messages[0].values())[0]
-        full_prompt = self.system_prompt.format(message)
-        return full_prompt
+        return self.translate_for_deepseek(messages)
     
     def parse_response(self, response: str) -> str:
         return response
+
+
+if __name__ == "__main__":
+    prompter = PlannerPrompt()
+    print(prompter.get_prompt([
+        {"role": "user", "content": "What is the sum of 2 and 2?"},
+        {"role": "assistant", "content": "The sum of 2 and 2 is 4"},
+        {"role": "user", "content": "What is the sum of 3 and 3?"},
+        {"role": "assistant", "content": "The sum of 3 and 3 is"}
+    ]))
--- src/aimo_gaz/prompts/prompt.py
+++ src/aimo_gaz/prompts/prompt.py
@@ -6,19 +6,48 @@ class Prompt(ABC):
         system_prompt_path: str = None, 
         example_prompt_path: str = None, 
         system_prompt: str = None, 
-        example_prompt: str = None):
-        assert not all([_ is None for _ in [system_prompt_path, system_prompt]]) and not all([_ is None for _ in [example_prompt_path, example_prompt]]), "Either system_prompt_path or system_prompt must be provided, and either example_prompt_path or example_prompt must be provided."
+        example_prompt: str = None,
+        append_system_prompt_after_every_message: bool = False
+    ):
+        # assert not all([_ is None for _ in [system_prompt_path, system_prompt]]) and not all([_ is None for _ in [example_prompt_path, example_prompt]]), "Either system_prompt_path or system_prompt must be provided, and either example_prompt_path or example_prompt must be provided."
         self.system_prompt_path = system_prompt_path
         self.example_prompt_path = example_prompt_path
         self.system_prompt = system_prompt
+        self.append_system_prompt_after_every_message = append_system_prompt_after_every_message
         self.example_prompt = example_prompt
-        if self.system_prompt is None:
+        if self.system_prompt is None and self.system_prompt_path is not None:
             with open(self.system_prompt_path, "r") as f:
                 self.system_prompt = f.read()
-        if self.example_prompt is None:
+        if self.example_prompt is None and self.example_prompt is not None:
             with open(self.example_prompt_path, "r") as f:
                 self.example_prompt = f.read()
-    
+
+
+    def translate_for_deepseek(self, messages):
+        last_role = None
+        seen_user_role_before = False
+
+        prompt = ''
+        for message in messages:
+            if message['role'] == 'user':
+
+                if last_role == 'assistant':
+                    prompt += '<｜end▁of▁sentence｜>'
+                if self.system_prompt is not None and (self.append_system_prompt_after_every_message or not seen_user_role_before):
+                    # prompt += f'User: {self.system_prompt}{message["content"]}\n'
+                    prompt += f"User: {self.system_prompt.format(message['content'], '{}').strip()}\n"
+                else:
+                    prompt += f'User: {message["content"].strip()}\n'
+                last_role = 'user'
+                seen_user_role_before = True
+            elif message['role'] == 'assistant':
+                prompt += f'Assistant: {message["content"].strip()}\n'
+                last_role = 'assistant'
+        if last_role == 'user':
+            prompt += 'Assistant:'
+        return prompt
+
+
     @abstractmethod
     def get_prompt(self, messages: typing.List[typing.Dict[str, str]]) -> str:
         pass
